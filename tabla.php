157cb5a869ad775c9b97dfd7980fd632a8b7b19c
Amilkar Cruz <amilkar.cruz@fundacion-jala.org>
2023-01-09 00:20:50 -0400

    Initial commit

---
+++ tabla.php
@@ -0,0 +1,13 @@
+<?php 
+echo '
+<tr>
+    <td>Contenido</td>
+    <td><input type="checkbox" name="foto_id" id="foto_id" class="check_foto" checked></td>
+    <td><input type="checkbox" name="entregado_id" id="entregado_id" class="check_entregado"></td>
+</tr>
+<tr>
+    <td>Contenido</td>
+    <td><input type="checkbox" name="foto_id" id="foto_id" class="check_foto"></td>
+    <td><input type="checkbox" name="entregado_id" id="entregado_id" class="check_entregado"></td>
+</tr>'
+?>
